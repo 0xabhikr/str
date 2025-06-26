c32c3ebc6541b4f74ecdb25a39be80fd6643b9f5
Abhikr-X | अभिषेक <abhikr6714@gmail.com>
2025-06-26 14:11:00 +0530

    Delete app directory

--- app/app/projects/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ProjectsPage() {
-  return (
-    <div className="mt-10">
-      <h1 className="text-2xl font-bold">Projects</h1>
-      <p>This is the Projects page.</p>
-    </div>
-  )
-}
