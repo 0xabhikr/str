f8f2218629c7a0f6c2c0a08d0d65912f7dbbebe4
Abhikr-X | अभिषेक <abhikr6714@gmail.com>
2025-06-26 13:44:48 +0530

    Delete app directory

--- app/friends/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function FriendsPage() {
-  return (
-    <div className="mt-10">
-      <h1 className="text-2xl font-bold">Friends</h1>
-      <p>This is the Friends page.</p>
-    </div>
-  )
-}
