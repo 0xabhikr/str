c2b827f9295a6e21b5ee10ac1950497364917ee4
Abhikr-X | अभिषेक <abhikr6714@gmail.com>
2025-06-26 14:11:46 +0530

    Delete app/about directory

--- app/about/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function AboutPage() {
-  return (
-    <div className="mt-10">
-      <h1 className="text-2xl font-bold">About Us</h1>
-      <p>This is the About page.</p>
-    </div>
-  )
-}
