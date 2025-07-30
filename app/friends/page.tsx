b69baf8fd70cda52b3e8de95256d9a4e721a9b61
Abhikr <abhikr6714@gmail.com>
2025-07-30 07:44:43 +0530

    some changes

--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -2,7 +2,7 @@ export default function FriendsPage() {
   return (
     <div className="mt-10">
       <h1 className="text-2xl font-bold">Friends</h1>
-      <p>This is the Friends page.</p>
+      <p>This the Friends page.</p>
     </div>
   )
 }
